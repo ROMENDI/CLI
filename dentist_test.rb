4f2841d72396b47cb95b9a4d15af1cc7eed4a745
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-01-23 06:32:39 +0000

    test cases added

---
+++ dentist_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'dentist_office'
+require_relative 'patient'
+
+class DentistOfficeTest < Minitest::Test
+  
+  def setup
+    @office = DentistOffice.new
+  end
+
+  def test_add_patient
+    @office.add_patient("Bob")
+    assert_includes @office.instance_variable_get(:@patients), "Bob"
+  end
+
+  def test_appointment_exists
+    @office.add_patient("Charlie")
+    patient = @office.find_patient("Charlie")
+    patient.schedule_appointment("2024-01-31", "Cleaning")
+    assert @office.appointment_exists?("2024-01-31")
+  end
+end
---
+++ patient_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'patient'
+
+class PatientTest < Minitest::Test
+
+  def setup
+    @patient = Patient.new("Bill")
+  end
+
+  def test_schedule_appointment
+    @patient.schedule_appointment("2024-01-30", "Checkup")
+    assert_equal 1, @patient.appointments.length
+    assert_equal "2024-01-30", @patient.appointments.first[:date]
+    assert_equal "Checkup", @patient.appointments.first[:procedure]
+  end
+  
+  def test_delete_appointment
+    @patient.schedule_appointment("2024-01-30", "Checkup")
+    @patient.delete_appointment("2024-01-30")
+    assert_empty @patient.appointments
+  end
+end
